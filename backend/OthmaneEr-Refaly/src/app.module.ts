fb9224fde03f31c4d33526b141d604e883dc6af8
Othmane Er-Refaly <oer-refa@c4r7p1.1337.ma>
2026-03-29 16:21:27 +0100

    [200~chore: Prepare database connections for merge

--- backend/OthmaneEr-Refaly/src/app.module.ts
+++ backend/OthmaneEr-Refaly/src/app.module.ts
@@ -1,12 +1,27 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
-import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({
-    isGlobal: true,
-  }), AuthModule],
-  controllers: [],
-  providers: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, 
+      envFilePath: '../../.env', // Pointing to the root .env
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        url: configService.get<string>('DATABASE_URL'), // Uses the URL from .env
+        autoLoadEntities: true, // FOr this one it will load my "Othmane" and your "zouita" entitys, just we need to make sure our entitys are the same.
+        synchronize: true, // I used this for  development. I might change it later to false and use migrations for production.
+      }),
+    }),
+    AuthModule,
+    UsersModule,
+  ],
 })
 export class AppModule {}
